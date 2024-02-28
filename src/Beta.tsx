705d49fcd9e83526f4a8d731df67b57bd797c68e
John Dana <john@danasd.com>
2024-02-27 17:07:07 -0700

    Minor Beta changes.

--- src/Beta.tsx
+++ src/Beta.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from "react";
 
+const myFunction = (myParam: string) => {
+  console.log(myParam);
+};
 const Beta: React.FC = () => {
+  myFunction("Hello");
   return (
     <div>
       <h1>Beta</h1>
